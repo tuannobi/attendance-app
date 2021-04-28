935af6a4630866a06914403a5eaebef077915f34
Tuan Tran <tuantrananhit@gmail.com>
2021-04-28 23:45:52 +0700

    merge successfully

--- Main.py
+++ Main.py
@@ -2,9 +2,9 @@ from view.LoginView import LoginView
 from view.SelectClassView import SelectClassView
 from service.Attendance import Attedance
 
-# login = LoginView()
-# login.showLoginGUI()
+login = LoginView()
+login.showLoginGUI()
 
-attendance = Attedance()
-attendance.takeAttendace("Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTYxODEyNjkwOCwiZXhwIjoxNjE4MTI3NTA4fQ.Q7h6pN4VQtPoeVH6I8s4DWCQjFDzpUowBrMYRX_6CgDgrtUjaO7UdEniHoJ3rYKS-k6eehIYJUDyVp3eFOu0Qw"
-,"CLASS1","17521284")
+# attendance = Attedance()
+# attendance.takeAttendace("Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJhZG1pbiIsImlhdCI6MTYxODEyNjkwOCwiZXhwIjoxNjE4MTI3NTA4fQ.Q7h6pN4VQtPoeVH6I8s4DWCQjFDzpUowBrMYRX_6CgDgrtUjaO7UdEniHoJ3rYKS-k6eehIYJUDyVp3eFOu0Qw"
+# ,"CLASS1","17521284")
